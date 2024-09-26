fc904374149d25067c757de63cb65912118b85d7
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-09-26 11:50:22 +0200

    Créaton methode hello()

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -9,13 +9,12 @@ public class App {
     public static void main(String[] args) {
         System.out.println(hello());
     }
-
+/**
+ * @author Cardouat Alexi
+ *  methode hello() sans paramettre retournant un String
+ * @return String "Hello World"
+ */  
     public static String hello() {
         return "Hello World!";
     }
-
-    public static String hello(String param) {
-        return param;
-    }
-
 }
